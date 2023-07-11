5a8f1969f02e5bbc146a8c64b32d74033eaf0939
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2023-07-11 10:44:11 -0300

    Fix module-info and build, update deps and plugins

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,11 @@
 module tn3270j {
+    requires static lombok;
+
+    requires java.logging;
+    requires bsh;
+    requires org.slf4j;
+    requires org.checkerframework.checker.qual;
+
     exports com.itquasar.multiverse.tn3270j;
     exports com.itquasar.multiverse.tn3270j.status;
 }
